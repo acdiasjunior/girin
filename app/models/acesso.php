0860550f40f3f6ec53058f527709bab570102c97
Junior Dias <acdiasjunior@gmail.com>
2012-05-01 14:33:10 -0300

    Renomeada tabela acessos > tb_acesso

--- app/models/acesso.php
+++ app/models/acesso.php
@@ -4,6 +4,8 @@ class Acesso extends AppModel {
 
     var $name = 'Acesso';
     var $primaryKey = 'id_acesso';
+    var $useTable = 'acesso';
+    var $tablePrefix = 'tb_';
     var $belongsTo = array(
         'Usuario' => array(
             'foreignKey' => 'id_usuario'
