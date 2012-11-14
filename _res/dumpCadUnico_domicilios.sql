449cd8f264bce37b15722923e9b9a5c1e1cff5f1
Junior <Junior@Laptospiroso>
2012-11-14 00:08:30 -0200

    Ajustes na importação de dados do cad_unico. Alterada consulta para importar dados de tabela incluida no proprio banco do programação local para importação

--- _res/dumpCadUnico_domicilios.sql
+++ _res/dumpCadUnico_domicilios.sql
@@ -1,81 +1,89 @@
 -- DOMICILIOS
--- bolsa_familia, `idf`, `data_pesquisa`, `data_inclusao`, `data_atualizacao`, `entrevistador`
-SELECT
-    DISTINCT ON (d.nu_domiciliar) d.nu_domiciliar AS codigo_domiciliar,
-    (SELECT DISTINCT(pp.co_nis) FROM cubtb027_pessoa pp INNER JOIN cubtb013_domicilio dd ON dd.co_domicilio = pp.co_domicilio
-        WHERE pp.nu_responsavel = pp.nu_pessoa AND dd.nu_domiciliar = d.nu_domiciliar AND (dt_exclusao_pessoa IS NULL OR dt_exclusao_pessoa = '1899-12-30') LIMIT 1) AS nis_responsavel,
-    d.co_cep_domicilio AS cep, d.ic_tipo_logradouro AS tipo_logradouro, d.no_logradouro_domicilio AS logradouro, d.co_logradouro_domicilio AS numero, d.de_complemento_logradouro_domicilio AS complemento,
-    d.no_bairro_domicilio AS bairro_nome, d.nu_ddd_domicilio AS ddd, d.nu_telefone_domicilio AS telefone, d.ic_tipo_localidade AS tipo_localidade, d.ic_situacao_domicilio AS situacao_domicilio,
-    d.ic_tipo_domicilio AS tipo_domicilio, d.ic_tipo_construcao AS tipo_construcao, d.ic_tipo_abastecimento_agua AS tipo_abastecimento, d.ic_tratamento_agua AS tratamento_agua,
-    d.ic_tipo_iluminacao AS tipo_iluminacao, d.ic_escoamento_sanitario AS escoamento_sanitario, d.ic_destino_lixo AS destino_lixo, d.qt_pessoas AS quantidade_pessoas, d.qt_comodos AS comodos,
-    -- Despesas
-    COALESCE(despesas.valor_despesa_aluguel,0) AS valor_despesa_aluguel, COALESCE(despesas.valor_despesa_prestacao,0) AS valor_despesa_prestacao,
-    COALESCE(despesas.valor_despesa_alimentacao,0) AS valor_despesa_alimentacao, COALESCE(despesas.valor_despesa_agua,0) AS valor_despesa_agua,
-    COALESCE(despesas.valor_despesa_luz,0) AS valor_despesa_luz, COALESCE(despesas.valor_despesa_transporte,0) AS valor_despesa_transporte,
-    COALESCE(despesas.valor_despesa_medicamento,0) AS valor_despesa_medicamento, COALESCE(despesas.valor_despesa_gas,0) AS valor_despesa_gas,
-    COALESCE(despesas.valor_outras_despesas,0) AS valor_outras_despesas,
-    (COALESCE(valor_despesa_aluguel,0) + COALESCE(valor_despesa_prestacao,0) + COALESCE(valor_despesa_alimentacao,0) + COALESCE(valor_despesa_agua,0) +
-    COALESCE(valor_despesa_luz,0) + COALESCE(valor_despesa_transporte,0) + COALESCE(valor_despesa_medicamento,0) + COALESCE(valor_despesa_gas,0) +
-    COALESCE(valor_outras_despesas,0)) AS valor_despesa_familia,
-    -- Rendas
-    COALESCE(rendas.valor_remuneracao,0) AS valor_remuneracao, COALESCE(rendas.valor_aposentadoria_pensao,0) AS valor_aposentadoria_pensao,
-    COALESCE(rendas.valor_seguro_desemprego,0) AS valor_seguro_desemprego, COALESCE(rendas.valor_pensao_alimenticia,0) AS valor_pensao_alimenticia,
-    COALESCE(rendas.valor_outras_rendas,0) AS valor_outras_rendas,
-    (COALESCE(valor_remuneracao,0) + COALESCE(valor_aposentadoria_pensao,0) +
-        COALESCE(valor_seguro_desemprego,0) + COALESCE(valor_pensao_alimenticia,0) +
-        COALESCE(valor_outras_rendas,0)) AS valor_renda_familia,
-    (CASE
-            WHEN d.dt_alteracao_domicilio = '1899-12-30' THEN '00/00/0000'
-            ELSE to_char(d.dt_alteracao_domicilio, 'DD/MM/YYYY')
-    END) AS data_atualizacao,
-    (CASE
-            WHEN d.dt_inclusao_domicilio = '1899-12-30' THEN '00/00/0000'
-            ELSE to_char(d.dt_inclusao_domicilio, 'DD/MM/YYYY')
-    END) AS data_inclusao,
-    (CASE
-            WHEN d.dt_pesquisa = '1899-12-30' THEN '00/00/0000'
-            ELSE to_char(d.dt_pesquisa, 'DD/MM/YYYY')
-    END) AS data_pesquisa
-FROM cubtb013_domicilio AS d
-INNER JOIN
-    cubtb027_pessoa AS p ON p.co_domicilio = d.co_domicilio
-INNER JOIN
-(SELECT
-	d.co_domicilio,
-	SUM(p.vr_despesa_aluguel) AS valor_despesa_aluguel,
-	SUM(p.vr_prestacao_habitacional) AS valor_despesa_prestacao,
-	SUM(p.vr_despesa_alimentacao) AS valor_despesa_alimentacao,
-	SUM(p.vr_despesa_agua) AS valor_despesa_agua,
-	SUM(p.vr_despesa_luz) AS valor_despesa_luz,
-	SUM(p.vr_despesa_transporte) AS valor_despesa_transporte,
-	SUM(p.vr_despesa_medicamento) AS valor_despesa_medicamento,
-	SUM(p.vr_despesa_gas) AS valor_despesa_gas,
-	SUM(p.vr_outras_despesas) AS valor_outras_despesas
-    FROM cubtb027_pessoa p
+INSERT INTO tb_domicilio (cod_domiciliar, cod_nis_responsavel, end_cep, end_tipo, end_logradouro, end_num, end_compl, nome_bairro,
+                            tel_ddd, tel_num, tp_localidade, tp_situacao_domicilio, tp_domicilio, tp_construcao, tp_abastecimento,
+                            tp_tratamento_agua, tp_iluminacao, tp_escoamento_sanitario, tp_destino_lixo, qtd_pessoa, qtd_comodo,
+                            vlr_despesa_aluguel, vlr_despesa_prestacao, vlr_despesa_alimentacao, vlr_despesa_agua, vlr_despesa_luz,
+                            vlr_despesa_transporte, vlr_despesa_medicamento, vlr_despesa_gas, vlr_outras_despesas, vlr_despesa_familia,
+                            vlr_remuneracao, vlr_aposentadoria_pensao, vlr_seguro_desemprego, vlr_pensao_alimenticia, vlr_outras_rendas,
+                            vlr_renda_familia, dt_atualizacao, dt_inclusao, dt_pesquisa)
+SELECT t.* FROM (
+    SELECT
+        DISTINCT ON (d.nu_domiciliar) d.nu_domiciliar AS cod_domiciliar,
+        (SELECT DISTINCT(pp.co_nis) FROM cubtb027_pessoa pp INNER JOIN cubtb013_domicilio dd ON dd.co_domicilio = pp.co_domicilio
+            WHERE pp.nu_responsavel = pp.nu_pessoa AND dd.nu_domiciliar = d.nu_domiciliar AND (dt_exclusao_pessoa IS NULL OR dt_exclusao_pessoa = '1899-12-30') LIMIT 1) AS cod_nis_responsavel,
+        d.co_cep_domicilio AS end_cep, d.ic_tipo_logradouro AS end_tipo, d.no_logradouro_domicilio AS end_logradouro, d.co_logradouro_domicilio AS end_num, d.de_complemento_logradouro_domicilio AS end_compl,
+        d.no_bairro_domicilio AS nome_bairro, substring(to_char(d.nu_ddd_domicilio, '999') from 1 for 3) AS tel_ddd, d.nu_telefone_domicilio AS tel_num, d.ic_tipo_localidade AS tp_localidade, d.ic_situacao_domicilio AS tp_situacao_domicilio,
+        d.ic_tipo_domicilio AS tp_domicilio, d.ic_tipo_construcao AS tp_construcao, d.ic_tipo_abastecimento_agua AS tp_abastecimento, d.ic_tratamento_agua AS tp_tratamento_agua,
+        d.ic_tipo_iluminacao AS tp_iluminacao, d.ic_escoamento_sanitario AS tp_escoamento_sanitario, d.ic_destino_lixo AS tp_destino_lixo, d.qt_pessoas AS qtd_pessoa, d.qt_comodos AS qtd_comodo,
+        -- Despesas
+        COALESCE(despesas.valor_despesa_aluguel,0) AS vlr_despesa_aluguel, COALESCE(despesas.valor_despesa_prestacao,0) AS vlr_despesa_prestacao,
+        COALESCE(despesas.valor_despesa_alimentacao,0) AS vlr_despesa_alimentacao, COALESCE(despesas.valor_despesa_agua,0) AS vlr_despesa_agua,
+        COALESCE(despesas.valor_despesa_luz,0) AS vlr_despesa_luz, COALESCE(despesas.valor_despesa_transporte,0) AS vlr_despesa_transporte,
+        COALESCE(despesas.valor_despesa_medicamento,0) AS vlr_despesa_medicamento, COALESCE(despesas.valor_despesa_gas,0) AS vlr_despesa_gas,
+        COALESCE(despesas.valor_outras_despesas,0) AS vlr_outras_despesas,
+        (COALESCE(valor_despesa_aluguel,0) + COALESCE(valor_despesa_prestacao,0) + COALESCE(valor_despesa_alimentacao,0) + COALESCE(valor_despesa_agua,0) +
+        COALESCE(valor_despesa_luz,0) + COALESCE(valor_despesa_transporte,0) + COALESCE(valor_despesa_medicamento,0) + COALESCE(valor_despesa_gas,0) +
+        COALESCE(valor_outras_despesas,0)) AS vlr_despesa_familia,
+        -- Rendas
+        COALESCE(rendas.valor_remuneracao,0) AS vlr_remuneracao, COALESCE(rendas.valor_aposentadoria_pensao,0) AS vlr_aposentadoria_pensao,
+        COALESCE(rendas.valor_seguro_desemprego,0) AS vlr_seguro_desemprego, COALESCE(rendas.valor_pensao_alimenticia,0) AS vlr_pensao_alimenticia,
+        COALESCE(rendas.valor_outras_rendas,0) AS vlr_outras_rendas,
+        (COALESCE(valor_remuneracao,0) + COALESCE(valor_aposentadoria_pensao,0) +
+            COALESCE(valor_seguro_desemprego,0) + COALESCE(valor_pensao_alimenticia,0) +
+            COALESCE(valor_outras_rendas,0)) AS vlr_renda_familia,
+        (CASE
+                WHEN d.dt_alteracao_domicilio = '1899-12-30' THEN NULL
+                ELSE d.dt_alteracao_domicilio
+        END) AS dt_atualizacao,
+        (CASE
+                WHEN d.dt_inclusao_domicilio = '1899-12-30' THEN NULL
+                ELSE d.dt_inclusao_domicilio
+        END) AS dt_inclusao,
+        (CASE
+                WHEN d.dt_pesquisa = '1899-12-30' THEN NULL
+                ELSE d.dt_pesquisa
+        END) AS dt_pesquisa
+    FROM cubtb013_domicilio AS d
     INNER JOIN
-             cubtb013_domicilio d ON p.co_domicilio = d.co_domicilio
-    GROUP BY
-	d.co_domicilio
-) AS despesas ON despesas.co_domicilio = d.co_domicilio
-INNER JOIN
-(SELECT
-	d.co_domicilio,
-        SUM(p.vr_remuneracao) AS valor_remuneracao,
-        SUM(p.vr_renda_aposentadoria) AS valor_aposentadoria_pensao,
-        SUM(p.vr_renda_seguro_desemprego) AS valor_seguro_desemprego,
-        SUM(p.vr_renda_pensao) AS valor_pensao_alimenticia,
-        SUM(p.vr_outras_rendas) AS valor_outras_rendas
-    FROM cubtb027_pessoa p
+        cubtb027_pessoa AS p ON p.co_domicilio = d.co_domicilio
     INNER JOIN
-             cubtb013_domicilio d ON p.co_domicilio = d.co_domicilio
-    GROUP BY
-	d.co_domicilio
-) AS rendas ON rendas.co_domicilio = d.co_domicilio
-WHERE
-    d.dt_pesquisa > '2009-09-01'
-    AND (d.dt_exclusao_domicilio = '1899-12-30' OR d.dt_exclusao_domicilio IS NULL)
-    AND d.qt_pessoas > 0
-    AND d.ic_situacao_cadastral = 'A'
-    AND d.ic_domicilio_valido = 't'
-    AND TRIM(d.no_logradouro_domicilio) <> '' AND d.no_logradouro_domicilio IS NOT NULL
-    AND (SELECT COUNT(*) FROM cubtb027_pessoa p WHERE p.co_domicilio = d.co_domicilio) = d.qt_pessoas
+    (SELECT
+            d.co_domicilio,
+            SUM(p.vr_despesa_aluguel) AS valor_despesa_aluguel,
+            SUM(p.vr_prestacao_habitacional) AS valor_despesa_prestacao,
+            SUM(p.vr_despesa_alimentacao) AS valor_despesa_alimentacao,
+            SUM(p.vr_despesa_agua) AS valor_despesa_agua,
+            SUM(p.vr_despesa_luz) AS valor_despesa_luz,
+            SUM(p.vr_despesa_transporte) AS valor_despesa_transporte,
+            SUM(p.vr_despesa_medicamento) AS valor_despesa_medicamento,
+            SUM(p.vr_despesa_gas) AS valor_despesa_gas,
+            SUM(p.vr_outras_despesas) AS valor_outras_despesas
+        FROM cubtb027_pessoa p
+        INNER JOIN
+                 cubtb013_domicilio d ON p.co_domicilio = d.co_domicilio
+        GROUP BY
+            d.co_domicilio
+    ) AS despesas ON despesas.co_domicilio = d.co_domicilio
+    INNER JOIN
+    (SELECT
+            d.co_domicilio,
+            SUM(p.vr_remuneracao) AS valor_remuneracao,
+            SUM(p.vr_renda_aposentadoria) AS valor_aposentadoria_pensao,
+            SUM(p.vr_renda_seguro_desemprego) AS valor_seguro_desemprego,
+            SUM(p.vr_renda_pensao) AS valor_pensao_alimenticia,
+            SUM(p.vr_outras_rendas) AS valor_outras_rendas
+        FROM cubtb027_pessoa p
+        INNER JOIN
+                 cubtb013_domicilio d ON p.co_domicilio = d.co_domicilio
+        GROUP BY
+            d.co_domicilio
+    ) AS rendas ON rendas.co_domicilio = d.co_domicilio
+    WHERE
+        d.dt_pesquisa > '2009-09-01'
+        AND (d.dt_exclusao_domicilio = '1899-12-30' OR d.dt_exclusao_domicilio IS NULL)
+        AND d.qt_pessoas > 0
+        AND d.ic_situacao_cadastral = 'A'
+        AND d.ic_domicilio_valido = 't'
+        AND TRIM(d.no_logradouro_domicilio) <> '' AND d.no_logradouro_domicilio IS NOT NULL
+        AND (SELECT COUNT(*) FROM cubtb027_pessoa p WHERE p.co_domicilio = d.co_domicilio) = d.qt_pessoas
+) AS t
--- _res/dumpCadUnico_pessoas.sql
+++ _res/dumpCadUnico_pessoas.sql
@@ -1,38 +1,45 @@
 -- PESSOAS
-SELECT
-    p.co_nis AS nis, d.nu_domiciliar AS codigo_domiciliar, p.no_pessoa AS nome, p.co_cpf AS cpf,
-    p.de_titulo_eleitor AS titulo_eleitor, p.de_zona_titulo_eleitor AS zona, p.de_secao_titulo_eleitor AS secao,
-    o.de_ocupacao AS ocupacao, p.ic_situacao_mercado AS tipo_trabalho,
+INSERT INTO tb_pessoa (cod_nis, cod_domiciliar, nome, cpf, teleitor_num, teleitor_zona,
+                        teleitor_secao, desc_ocupacao, tp_trabalho, dt_nasc, cod_inep, cod_nis_responsavel,
+                        tp_pararentesco_responsavel, vlr_remuneracao, vlr_aposentadoria, vlr_seguro_desemprego, 
+                        vlr_pensao, vlr_outra_renda, serie_escolar, grau_instrucao, tp_escola, sexo, raca, est_civil,
+                        qtd_mes_gestacao, amamentando, cegueira, mudez, surdez, deficiencia_mental, deficiencia_fisica, 
+                        outra_deficiencia, portador_deficiencia, dt_atualizacao, dt_inclusao, esposa_companheiro)
+SELECT t.* FROM
+(SELECT
+    DISTINCT ON (p.co_nis) p.co_nis AS cod_nis, d.nu_domiciliar AS cod_domiciliar, p.no_pessoa AS nome, p.co_cpf AS cpf,
+    p.de_titulo_eleitor AS teleitor_num, p.de_zona_titulo_eleitor AS teleitor_zona, p.de_secao_titulo_eleitor AS teleitor_secao,
+    substring(o.de_ocupacao FROM 1 FOR 30) AS desc_ocupacao, p.ic_situacao_mercado AS tp_trabalho,
     (CASE
-        WHEN p.dt_nascimento = '1899-12-30' THEN '00/00/0000'
-        ELSE to_char(p.dt_nascimento, 'DD/MM/YYYY')
-    END) AS data_nascimento,
-    p.nu_inep_escola AS inep, r.co_nis AS responsavel_nis, COALESCE(p.ic_parentesco_responsavel,20) AS responsavel_parentesco,
-    COALESCE(p.vr_remuneracao,0) AS valor_remuneracao,
-    COALESCE(p.vr_renda_aposentadoria,0) AS valor_aposentadoria, COALESCE(p.vr_renda_seguro_desemprego,0) AS valor_seguro_desemprego,
-    COALESCE(p.vr_renda_pensao,0) AS valor_pensao, COALESCE(p.vr_outras_rendas,0) AS valor_outras_rendas,
+        WHEN p.dt_nascimento = '1899-12-30' THEN NULL
+        ELSE p.dt_nascimento
+    END) AS dt_nasc,
+    p.nu_inep_escola AS cod_inep, r.co_nis AS cod_nis_responsavel, COALESCE(p.ic_parentesco_responsavel,20) AS tp_pararentesco_responsavel,
+    COALESCE(p.vr_remuneracao,0) AS vlr_remuneracao,
+    COALESCE(p.vr_renda_aposentadoria,0) AS vlr_aposentadoria, COALESCE(p.vr_renda_seguro_desemprego,0) AS vlr_seguro_desemprego,
+    COALESCE(p.vr_renda_pensao,0) AS vlr_pensao, COALESCE(p.vr_outras_rendas,0) AS vlr_outra_renda,
     COALESCE(p.ic_serie_escolar,0) AS serie_escolar, COALESCE(p.ic_grau_instrucao,0) AS grau_instrucao,
-    COALESCE(p.ic_tipo_escola,0) AS tipo_escola, p.ic_sexo AS genero, p.ic_raca_cor AS raca_cor, p.ic_estado_civil AS estado_civil,
-    COALESCE(p.nu_mes_gestacao,0) AS mes_gestacao,
+    COALESCE(p.ic_tipo_escola,0) AS tp_escola, to_number(p.ic_sexo, '9') AS sexo, p.ic_raca_cor AS raca, p.ic_estado_civil AS est_civil,
+    COALESCE(p.nu_mes_gestacao,0) AS qtd_mes_gestacao,
     (CASE
         WHEN p.ic_amamentando IS NULL THEN 0
         WHEN p.ic_amamentando = '2' THEN 0
         ELSE 1
     END) AS amamentando,
-    p.ic_cegueira AS cegueira, p.ic_mudez AS mudez, p.ic_surdez AS surdez,
-    p.ic_deficiencia_mental AS deficiencia_mental, p.ic_deficiencia_fisica AS deficiencia_fisica, p.ic_outra_deficiencia AS outra_deficiencia,
+    to_number(p.ic_cegueira, '9') AS cegueira, to_number(p.ic_mudez, '9') AS mudez, to_number(p.ic_surdez, '9') AS surdez,
+    to_number(p.ic_deficiencia_mental, '9') AS deficiencia_mental, to_number(p.ic_deficiencia_fisica, '9') AS deficiencia_fisica, to_number(p.ic_outra_deficiencia, '9') AS outra_deficiencia,
     (CASE
         WHEN p.ic_sem_deficiencia = '0' THEN 1
         ELSE 0
      END) AS portador_deficiencia,
     (CASE
-        WHEN p.dt_alteracao_pessoa = '1899-12-30' THEN '00/00/0000'
-        ELSE to_char(p.dt_alteracao_pessoa, 'DD/MM/YYYY')
-    END) AS data_atualizacao,
+        WHEN p.dt_alteracao_pessoa = '1899-12-30' THEN NULL
+        ELSE p.dt_alteracao_pessoa
+    END) AS dt_atualizacao,
     (CASE
-        WHEN p.dt_inclusao_pessoa = '1899-12-30' THEN '00/00/0000'
-        ELSE to_char(p.dt_inclusao_pessoa, 'DD/MM/YYYY')
-    END) AS data_inclusao,
+        WHEN p.dt_inclusao_pessoa = '1899-12-30' THEN NULL
+        ELSE p.dt_inclusao_pessoa
+    END) AS dt_inclusao,
     (CASE
         WHEN p.nu_ordem_esposa_companheiro <> 99 THEN 1
         ELSE 0
@@ -48,15 +55,8 @@ LEFT JOIN
     cubtb038_ocupacao AS o
     ON p.co_ocupacao = o.co_ocupacao
 WHERE
-    p.co_domicilio IN
-    (SELECT d.co_domicilio FROM cubtb013_domicilio AS d
-        WHERE d.dt_pesquisa >= '2009-09-01'
-            AND (d.dt_exclusao_domicilio = '1899-12-30' OR d.dt_exclusao_domicilio IS NULL)
-            AND d.qt_pessoas > 0
-            AND d.ic_situacao_cadastral = 'A'
-            AND d.ic_domicilio_valido = 't'
-            AND TRIM(d.no_logradouro_domicilio) <> '' AND d.no_logradouro_domicilio IS NOT NULL
-            AND (SELECT COUNT(*) FROM cubtb027_pessoa p WHERE p.co_domicilio = d.co_domicilio) = d.qt_pessoas
-    )
+    d.nu_domiciliar IN (SELECT DISTINCT(td.cod_domiciliar::numeric) FROM tb_domicilio td)
     AND p.co_nis IS NOT NULL
     AND (p.dt_exclusao_pessoa = '1899-12-30' OR p.dt_exclusao_pessoa IS NULL)
+    AND p.no_pessoa IS NOT NULL
+) AS t
